2f131a78e8e04e99d0163f7f59d99976f10421ac
Xetera <contact@xetera.dev>
2026-01-22 17:37:29 +0300

    feat: add `DisabledIndexes` class

---
+++ src/remote/disabled-indexes.ts
@@ -0,0 +1,36 @@
+import { PgIdentifier } from "@query-doctor/core";
+
+/**
+ * A class representing disabled indexes for the
+ * sole purpose of exposing a {@link PgIdentifier} interface.
+ */
+export class DisabledIndexes {
+  private readonly disabledIndexNames = new Set<string>();
+
+  add(indexName: PgIdentifier): void {
+    this.disabledIndexNames.add(indexName.toString());
+  }
+
+  remove(indexName: PgIdentifier): boolean {
+    return this.disabledIndexNames.delete(indexName.toString());
+  }
+
+  /**
+   * Toggles the visibility of the index
+   * @returns did the index get disabled?
+   */
+  toggle(indexName: PgIdentifier): boolean {
+    const deleted = this.remove(indexName);
+    if (!deleted) {
+      this.add(indexName);
+      return false;
+    }
+    return true;
+  }
+
+  [Symbol.iterator](): Iterator<PgIdentifier> {
+    return this.disabledIndexNames.values().map((indexName) =>
+      PgIdentifier.fromString(indexName)
+    );
+  }
+}
