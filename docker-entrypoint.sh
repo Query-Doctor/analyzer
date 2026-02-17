014811043875f90bb8ce79dcad1529db13561716
Xetera <contact@xetera.dev>
2026-02-17 20:52:11 +0300

    fix: dockerfile build

---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "listen_addresses = '0.0.0.0'" >> "$PGDATA/postgresql.conf"
+echo "host all all 0.0.0.0/0 trust" >> "$PGDATA/pg_hba.conf"
+echo "shared_preload_libraries = 'timescaledb,pg_stat_statements'" >> "$PGDATA/postgresql.conf"
+
+pg_ctl -D "$PGDATA" -l "$PGDATA/logfile" start || (cat "$PGDATA/logfile" && exit 1)
+
+until pg_isready -h /tmp; do sleep 0.5; done
+
+node /app/dist/main.mjs
